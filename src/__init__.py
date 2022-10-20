e9dad06edcdea5a733cb50ec0ca9b0ce24057617
Giulio Vaccari <giulio09@marcov.it>
2022-10-20 19:51:02 +0200

    First commit

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+from tfest.tfest import tfest
+
+__version__ = '0.0.1'
+__author__ = 'PoliDuckies'
+__email__ = 'duckietown@aeapolimi.it'
+__license__ = 'MIT'
+__url__ = 'github.com/poliduckies/poliduckie_segmentation'
