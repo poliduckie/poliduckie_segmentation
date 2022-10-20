0bbfb58c4ad9fb310d5c5d90a74cc3858cca4209
Giulio Vaccari <giulio09@marcov.it>
2022-10-20 20:24:10 +0200

    With relative path

--- build/lib/poliduckie_segmentation/segmentation.py
+++ build/lib/poliduckie_segmentation/segmentation.py
@@ -1,10 +1,10 @@
-import tensorflow as tf
-try:
-    import importlib.resources as pkg_resources
-except ImportError:
-    import importlib_resources as pkg_resources
+import os
 
-# from poliduckie_segmentation import model
+import tensorflow as tf
+# try:
+#     import importlib.resources as pkg_resources
+# except ImportError:
+#     import importlib_resources as pkg_resources
 
 IMG_WIDTH = 320
 IMG_HEIGHT = 240
@@ -12,10 +12,11 @@ IMG_CHANNELS = 3
 
 class Segmentation:
     def __init__(self):
-        pass
-        # model_path = pkg_resources.path(model, 'multiclass_segmentation_model')
-        # self.model = tf.keras.models.load_model(model_path)
-        # self.model.summary()
+        # can be done better with importlib.resources
+        pkg_path = os.path.abspath(__file__)
+        model_path = os.path.join(os.path.dirname(pkg_path), 'model')
+        self.model = tf.keras.models.load_model(model_path)
+        self.model.summary()
 
     def predict(self, image):
         return self.model.predict(image.reshape((1,IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS)))
