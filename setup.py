2d6e9da225ee34f0f2c7ce78da3870ccf5e8da57
Giulio Vaccari <giulio09@marcov.it>
2022-10-23 17:36:22 +0200

    With MPC model ready

--- setup.py
+++ setup.py
@@ -17,7 +17,9 @@ setuptools.setup(
   # download_url = 'https://github.com/poliduckies/poliduckie_segmentation/archive/refs/tags/v'+version+'-alpha.tar.gz',
   keywords = ['segmentation', 'duckietown', 'tensorflow'],
   install_requires=[
-          'tensorflow'
+          'tensorflow',
+          'casadi',
+          'numpy'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
