b0d3caf92c079c54fa1e0f2934d25db0e4ccf932
Giulio Vaccari <44315076+giuliovv@users.noreply.github.com>
2022-10-25 18:56:25 +0200

    With linear and angular speeds

--- poliduckie_segmentation/control.py
+++ poliduckie_segmentation/control.py
@@ -10,11 +10,12 @@ class MPC:
         self.model = ca.Function.load(model_path)
         self.delay = round(0.15/dt)
         self.u_delay0 = ca.DM(np.zeros((2, self.delay)))
+        self.wheel_speed = wheel_speed
 
     def get_model(self):
         return self.model
 
-    def mpc(self, x, r, tr=None, u_delay=None,  Q1=1e3, Q2=5e-4, Q3=1, R=1e-3):
+    def mpc_wheel_speed(self, x, r, tr=None, u_delay=None,  Q1=1e3, Q2=5e-4, Q3=1, R=1e-3):
         """
         x: state
         r: reference
@@ -33,3 +34,22 @@ class MPC:
         if u_delay is None:
             u_delay = self.u_delay0
         return np.around(self.model(x, r, tr, u_delay, Q1, Q2, Q3, R).toarray().reshape(-1), 2)
+    
+    def mpc(self, x, r, tr=None, u_delay=None,  Q1=1e3, Q2=5e-4, Q3=1, R=1e-3):
+        """
+        x: state
+        r: reference
+        tr: angular reference
+        u_delay: delayed control input
+        Q1: weight on position error
+        Q2: weight on angular error
+        Q3: weight on max speed
+        R: weight on control input
+        
+        returns: [linear speed, angular speed]
+        """
+        left_speed, right_speed = self.mpc_wheel_speed(x, r, tr=tr, u_delay=u_delay,  Q1=Q1, Q2=Q2, Q3=Q3, R=R)
+        linear_speed = (left_speed + right_speed)/2
+        angular_speed = (right_speed - left_speed)/0.10467
+        
+        return np.array([linear_speed, angular_speed])
