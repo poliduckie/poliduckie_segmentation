54da551e5d059bb104e715f5171a3ccce66b7d96
Giulio Vaccari <44315076+giuliovv@users.noreply.github.com>
2022-11-09 21:07:11 +0100

    Update requirements

--- setup.py
+++ setup.py
@@ -19,7 +19,8 @@ setuptools.setup(
   install_requires=[
           'tensorflow',
           'casadi',
-          'numpy'
+          'numpy',
+          'cv2'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
